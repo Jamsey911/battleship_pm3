7483390538641eb5f1d2b960544dbaa0a7d4e801
Jamsey911 <jamesalynch911@hotmail.com>
2023-01-31 21:50:18 +0000

    inital commit

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("  A B C D E F G H I J")
+for x in range(10):
+    print(x,"_ " * 10)
