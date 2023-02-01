1258538d8d134017a3099742ac0e02f094a4e688
Jamsey911 <jamesalynch911@hotmail.com>
2023-02-01 00:34:39 +0000

    Add Check_shot & get_shot

--- run.py
+++ run.py
@@ -1,3 +1,64 @@
-print("  A B C D E F G H I J")
-for x in range(10):
-    print(x,"_ " * 10)
+def get_shot(guesses):
+    ok = "n"
+    while ok == "n":
+        try:
+            shot = input("Please enter your guess: ")
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("incorrect number, please try again")
+            elif shot in guesses:
+                print("Number already guessed")
+            else:
+                ok = "y"
+                break
+        except Exception:
+            print("Incorrect entry, please select again")
+    return shot
+
+
+def show_board(hit, miss, sink):
+    print("          Battleships")
+    print("     A  B  C  D  E  F  G  H  I  J")
+
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _ "
+            if place in miss:
+                ch = " o "
+            elif place in hit:
+                ch = " x "
+            elif place in sink:
+                ch = " S "
+            row = row + ch
+            place = place + 1
+        print(x, " ", row)
+
+
+def check_shot(shot, boat1, hit, miss, sink):
+    if shot in boat1:
+        boat1.remove(shot)
+        if len(boat1) > 0:
+            hit.append(shot)
+            print("You Hit")
+        else:
+            sink.append(shot)
+            print("You sunk a ship")
+    else:
+        miss.append(shot)
+        print("You missed")
+
+    return boat1, hit, miss, sink
+
+
+boat1 = [45, 46, 47]
+hit = []
+miss = []
+sink = []
+
+for i in range(10):
+    guesses = hit + miss + sink
+    shot = get_shot(guesses)
+    boat1, hit, miss, sink = check_shot(shot, boat1, hit, miss, sink)
+    show_board(hit, miss, sink)
