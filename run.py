805703d57fa10b6dfb12457eccbb7f8b622963e5
Jamsey911 <jamesalynch911@hotmail.com>
2023-02-02 18:26:43 +0000

    Add boat2 and boat 3

--- run.py
+++ run.py
@@ -36,10 +36,26 @@ def show_board(hit, miss, sink):
         print(x, " ", row)
 
 
-def check_shot(shot, boat1, hit, miss, sink):
+def check_shot(shot, boat1, boat2, boat3, hit, miss, sink):
     if shot in boat1:
         boat1.remove(shot)
         if len(boat1) > 0:
+            hit.append(shot)
+            print("You Hit")
+        else:
+            sink.append(shot)
+            print("You sunk a ship")   
+    elif shot in boat2:
+        boat2.remove(shot)
+        if len(boat2) > 0:
+            hit.append(shot)
+            print("You Hit")
+        else:
+            sink.append(shot)
+            print("You sunk a ship")
+    elif shot in boat3:
+        boat3.remove(shot)
+        if len(boat3) > 0:
             hit.append(shot)
             print("You Hit")
         else:
@@ -49,10 +65,13 @@ def check_shot(shot, boat1, hit, miss, sink):
         miss.append(shot)
         print("You missed")
 
-    return boat1, hit, miss, sink
+    return boat1, boat2, boat3, hit, miss, sink
 
 
 boat1 = [45, 46, 47]
+boat2 = [12, 13, 14, 15]
+boat3 = [23, 24]
+
 hit = []
 miss = []
 sink = []
@@ -60,5 +79,11 @@ sink = []
 for i in range(10):
     guesses = hit + miss + sink
     shot = get_shot(guesses)
-    boat1, hit, miss, sink = check_shot(shot, boat1, hit, miss, sink)
+    boat1, boat2, boat3, hit, miss, sink = check_shot(shot, boat1, boat2, 
+        boat3, hit, miss, sink)
     show_board(hit, miss, sink)
+
+    if len(boat1) < 1 and len(boat2) < 1 and len(boat3) < 1:
+        print("You've won")
+        break
+print("Finished")
